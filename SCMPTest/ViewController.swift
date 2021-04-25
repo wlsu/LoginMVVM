c11c2f306a45768e42160d306c405003cb486b83
vincentsu <vincentsu.wl@gmail.com>
2021-04-25 22:33:39 +0800

    Init commit and build succeed

---
+++ SCMPTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SCMPTest
+//
+//  Created by Vincent Su on 25/4/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
